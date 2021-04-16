171c7575e8be1b6c1e001a0372fbca849eb78c1a
David Mukhiya <sherlock.41v94@gmail.com>
2021-04-15 17:44:00 -0700

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-$(function() {
-    $(".carousel").carousel( { interval: 2000, pause: "false" } );
-    $("#carouselButton").click(function(){
+//for Carosuel
+
+$(function () {
+    $(".carousel").carousel({ interval: 2000, pause: "false" });
+    $("#carouselButton").click(function () {
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
@@ -8,7 +10,22 @@ $(function() {
         } else {
             $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause"); 
+            $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
 });
+
+//for Reserve Modal
+$(function () {
+    $('#reserveButton').click(function () {
+        $('#reserveModal').modal('show');
+    });
+});
+
+//for Login Modal
+$(function () {
+    $('#loginButton').click(function () {
+        $('#loginModal').modal('show');
+    });
+});
+
